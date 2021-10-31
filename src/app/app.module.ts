32c9d01f5dc3fd9dd6ee1c5bf3154448c02fce42
dgrivov <grivov7858@gmail.com>
2021-10-31 16:24:31 +0300

    Added front end skeleton of restaurant menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RatingModule } from 'ng-starrating';
+import { FormsModule } from '@angular/forms';
+import { TagsComponent } from './components/tags/tags.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { FoodPageComponent } from './components/food-page/food-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    TagsComponent,
+    FoodPageComponent,
+    CartPageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RatingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
